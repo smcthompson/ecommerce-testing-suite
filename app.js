5b374851c076fb055c609fd0789a51463f2ac5ff
Scott Thompson <iiicubed.com@gmail.com>
2025-06-08 22:31:07 -0500

    Removed the `port` from the `origin`...
    ...to avoid CORS errors with `fetch` versus IIS loads.

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS middleware
 app.use(cookieParser());
 app.use(cors({
-  origin: `${protocol}://${host}:${port}`,
+  origin: `${protocol}://${host}`,
   credentials: true,
 }));
 // Compression middleware
