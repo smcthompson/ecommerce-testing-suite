9f1991f837fcd06ac9725744c5594929524244a1
Scott Thompson <iiicubed.com@gmail.com>
2025-07-12 21:57:47 -0500

    Update Vite config...
    ...to use folder structure best practices.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
+  publicDir: 'public',
   build: {
-    outDir: 'public',
-    emptyOutDir: false,
+    outDir: 'dist',
+    emptyOutDir: true,
   },
+  root: '.',
 });
