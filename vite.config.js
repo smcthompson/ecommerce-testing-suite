177bd231bc2291fdcfa1cd16355e62eb880b6280
Scott Thompson <iiicubed.com@gmail.com>
2025-07-14 19:58:41 -0500

    Updated configurations...
    ...to allow for HMR development and debugging.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  server: {
+    port: 5173, // Vite default, or your preferred port
+    proxy: {
+      '/api': {
+        target: 'https://localhost:3000',
+        secure: false,
+      }
+    },
+  },
   publicDir: 'public',
   build: {
     outDir: 'dist',
